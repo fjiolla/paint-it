7371fa9352667143cebe7405de4d2f240592df35
Leena Shah <leenashah065@gmail.com>
2025-11-12 00:36:21 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import ColoringBoard from './ColorBoard'
+import './App.css'
+
+function App() {
+  return (
+    <ColoringBoard/>
+  );
+}
+
+export default App
---
+++ src/ColorBoard.jsx
@@ -0,0 +1,67 @@
+import React, { useEffect, useState, useRef } from "react";
+import { colors } from "./ColorPalette";
+
+const ColoringBoard = () => {
+  const [selectedColor, setSelectedColor] = useState("#ff0000");
+  const svgContainerRef = useRef(null);
+
+  useEffect(() => {
+    fetch("/test.svg")
+      .then((res) => res.text())
+      .then((data) => {
+        if (svgContainerRef.current) {
+          svgContainerRef.current.innerHTML = data;
+        }
+      });
+  }, []);
+
+  const handleClick = (e) => {
+    if (e.target.tagName === "path" || e.target.tagName === "circle") {
+      e.target.style.fill = selectedColor;
+    }
+  };
+
+  const handleColorSelect = (color) => {
+    setSelectedColor(color);
+  };
+
+  return (
+    <div>
+      <h1>Creative Coloring Board</h1>
+
+      <h3>Color Palette</h3>
+      <div>
+        {colors.map((color) => (
+          <button
+            key={color}
+            onClick={() => handleColorSelect(color)}
+            style={{
+              margin: "5px 5px 30px 5px",
+              backgroundColor: color,
+              border: selectedColor === color ? "2px solid #E5E7EB" : "",
+            }}
+          />
+        ))}
+      </div>
+
+      <div
+        ref={svgContainerRef}
+        style={{
+          width: "100%",
+          padding: "20px",
+          background: "linear-gradient(145deg, #ffffff, #f5f5f5)",
+          borderRadius: "16px",
+          display: "flex",        
+          justifyContent: "center", 
+          alignItems: "center",
+        }}
+        onClick={handleClick}
+      />
+
+      
+    </div>
+  );
+};
+
+export default ColoringBoard;
+
